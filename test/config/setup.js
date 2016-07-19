052f68ea221ac46de7b551412d401b811a764133
Nick Lee <nick@tendigi.com>
2016-07-19 12:54:36 -0400

    set up temporary environments for tests

--- test/config/setup.js
+++ test/config/setup.js
@@ -1,21 +1,35 @@
 'use strict';
 
-const bootstrap = require('./bootstrap');
+const path = require('path');
+const fs = require('fs-extra');
 const supertest = require('supertest');
+const bootstrap = require('./bootstrap');
 
 module.exports = (before, after) => {
     
     let doneCallback;
 
+    const env = 'basic';
+
+    const envSource = path.join(__filename, '../../env_source/', env);
+    const tempEnv = path.join(__filename, '../../env/', env);
+
     before(function (done) {
-        bootstrap('../envs/basic/environment.json', '../envs/basic/resources.json', teardown => {
+
+        fs.removeSync(tempEnv);
+        fs.copySync(envSource, tempEnv);
+
+        bootstrap('../env/' + env + '/environment.json', '../env/' + env + '/resources.json', teardown => {
             doneCallback = teardown;
             done();
         });
     });
 
     after(function (done) {
-        doneCallback(done);
+        doneCallback(err => {
+            fs.removeSync(tempEnv);
+            done(err);
+        });
     });
 
     return supertest('http://127.0.0.1:8000');
