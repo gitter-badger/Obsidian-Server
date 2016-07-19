3ec26d01bab6cd4a2a2389f4ecebf686565aec15
Nick Lee <nick@tendigi.com>
2016-07-19 12:26:27 -0400

    updated tests

---
+++ test/response.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+
+module.exports = res => {
+    const keys = ['_requestID', '_requestTimestamp', '_responseTimestamp'];
+    _.each(keys, key => {
+        if (!(key in res.body)) throw new Error("missing " + key + " key");
+        delete res.body[key];
+    });
+};
